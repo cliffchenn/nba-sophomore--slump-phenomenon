e646efa44c68c89087135ea44b8cc186e57ee6c5
unknown <c424chen@uwaterloo.ca>
2020-12-15 22:53:50 -0800

    percent difference between average and player performance index

---
+++ secrets.py
@@ -0,0 +1,38 @@
+best_urls = ["https://www.basketball-reference.com/players/i/irvinky01.html",
+             "https://www.basketball-reference.com/players/d/davisan02.html",
+             "https://www.basketball-reference.com/players/c/cartevi01.html",
+             "https://www.basketball-reference.com/players/j/jamesle01.html",
+             "https://www.basketball-reference.com/players/d/doncilu01.html",
+             "https://www.basketball-reference.com/players/m/mingya01.html"
+
+]
+
+best_players = ["Kyrie Irving",
+                "Anthony Davis",
+                "Vince Carter",
+                "Lebron James",
+                "Luka Doncic",
+                "Yao Ming"
+]
+
+
+worst_urls = ["https://www.basketball-reference.com/players/c/clarkjo01.html",
+              "https://www.basketball-reference.com/players/m/mudiaem01.html",
+              "https://www.basketball-reference.com/players/o/okafoja01.html",
+              "https://www.basketball-reference.com/players/b/battish01.html",
+              "https://www.basketball-reference.com/players/e/evansty01.html",
+              "https://www.basketball-reference.com/players/c/cartemi01.html"
+
+
+]
+
+worst_players = ["Jordan Clarkson",
+                 "Emmanuel Mudiay",
+                 "Jahil Okafor",
+                 "Shane Battier",
+                 "Tyreke Evans",
+                 "Michael Carter-Williams"
+]
+
+
+
